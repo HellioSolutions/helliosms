a40f7d2a0709e50f63761988bae1f335f94d4307
Albert Ninyeh <eaglesecurity0@gmail.com>
2022-08-02 12:23:48 +0000

    added more code to exceptions

--- src/Exceptions/ValidationException.php
+++ src/Exceptions/ValidationException.php
@@ -7,5 +7,10 @@ use Exception;
 class ValidationException extends Exception
 {
 
+    public function __construct($message, $code = 0, Exception $previous = null)
+    {
+        parent::__construct($message, $code, $previous);
+    }
+
 
 }
