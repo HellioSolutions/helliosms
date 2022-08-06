7eb9b06d9d73cec98ced7bd7364cbc186e4708fd
Norris Oduro <norrisjibril@gmail.com>
2022-08-06 20:42:36 +0000

    Fix config publish bug

--- src/HellioMessagingServiceProvider.php
+++ src/HellioMessagingServiceProvider.php
@@ -5,7 +5,6 @@ namespace Hellio\HellioMessaging;
 use Hellio\HellioMessaging\Channels;
 use Hellio\HellioMessaging\Client;
 use Illuminate\Support\Arr;
-use Illuminate\Support\Str;
 use Illuminate\Support\Facades\Notification;
 use Illuminate\Support\Facades\Validator;
 use Illuminate\Support\ServiceProvider;
@@ -13,15 +12,18 @@ use Illuminate\Support\ServiceProvider;
 class HellioMessagingServiceProvider extends ServiceProvider
 {
 
-      /**
+    /**
      * Bootstrap services.
      *
      * @return void
      */
     public function boot()
     {
-            $this->mergeConfigFrom(__DIR__.'/../config/helliomessaging.php', 'helliomessaging');
-            $this->publishAssets();
+        if ($this->app->runningInConsole()) {
+            $this->publishes([
+                __DIR__ . '/../config/helliomessaging.php' => config_path('helliomessaging.php')
+            ], 'config');
+        }
 
         Validator::extend('hellio_otp', function ($attribute, $value, $parameters, $validator) {
             $client = app(Client::class);
@@ -31,18 +33,8 @@ class HellioMessagingServiceProvider extends ServiceProvider
         });
     }
 
-       protected function publishAssets()
-    {
-        if (!$this->app->runningInConsole() || Str::contains($this->app->version(), 'lumen'))
-            return;
-
-        $this->publishes([__DIR__.'/../config/helliomessaging.php' => config_path('helliomessaging.php')]);
-    }
-
-
     public function register()
     {
-        $this->mergeConfigFrom(__DIR__ . '/../config/helliomessaging.php', 'helliomessaging');
         $this->app->bind(Client::class, function () {
             return new Client(env('helliomessaging.client_id'), env('helliomessaging.application_secret'));
         });
@@ -52,8 +44,7 @@ class HellioMessagingServiceProvider extends ServiceProvider
         });
     }
 
-
-     public function provides()
+    public function provides()
     {
         return ['helliomessaging'];
     }
